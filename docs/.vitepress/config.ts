c20d497a27b529a151c0a5b0f509f25cb6704076
Edward West <edward_job@163.com>
2023-05-04 09:33:14 +0800

    :sparkles: feat: 初始化 VitePress 项目，自定义配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
   title: "My Refreshed Space",
   description: "A coding and record space",
   base: '',
-  
+  // head: [ ],
+  // rewrites: {
+  //   'source/:page': 'destination/:page'
+  // },
+  // srcExclude: ['**/README.md', '**/TODO.md'],
+  // outDir: './.vitepress/dist', // default
+  // cacheDir: './.vitepress/.vite', // default
+  // ignoreDeadLinks: true, // default false
 })
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,11 @@
 ---
-home: true
----
+layout: home
 
+hero:
+  name: VitePress
+  text: Vite & Vue powered static site generator.
+  tagline: Lorem ipsum...
+  image:
+    src: /logo.png
+    alt: 网页的logo图标
+---
